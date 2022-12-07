e2935b68563a2938201f3a5b63b4829f3c308d4c
kwietelmann <kaw57@calvin.edu>
2022-12-07 11:48:38 -0500

    attempt at getting MQTT working with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IMqttMessage, MqttService } from 'ngx-mqtt';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'KnightWash-WebUI';
+  private subscription: Subscription;
+  topicname: any;
+  msg: any;
+  isConnected: boolean = false;
+  @ViewChild('msglog', { static: true }) msglog: ElementRef;
+
+  constructor(private _mqttService: MqttService) { }
+
+  ngOnInit(): void { }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+
+  subscribeNewTopic(): void {
+    console.log('inside subscribe new topic')
+    this.subscription = this._mqttService.observe(this.topicname).subscribe((message: IMqttMessage) => {
+      this.msg = message;
+      console.log('msg: ', message)
+      this.logMsg('Message: ' + message.payload.toString() + '<br> for topic: ' + message.topic);
+    });
+    this.logMsg('subscribed to topic: ' + this.topicname)
+  }
+
+  sendmsg(): void {
+    // use unsafe publish for non-ssl websockets
+    this._mqttService.unsafePublish(this.topicname, this.msg, { qos: 1, retain: true })
+    this.msg = ''
+  }
+
+  logMsg(message: string): void {
+    this.msglog.nativeElement.innerHTML += '<br><hr>' + message;
+  }
+
+  clear(): void {
+    this.msglog.nativeElement.innerHTML = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 
 // import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from '@angular/material';
@@ -14,19 +14,29 @@ import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/lega
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MqttModule, IMqttServiceOptions } from "ngx-mqtt";
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: 'test.mosquitto.org',
+  port: 8080,
+  path: '/calvin/beta/washer/test'
+}
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS),
   ],
   providers: [],
   bootstrap: [AppComponent]
