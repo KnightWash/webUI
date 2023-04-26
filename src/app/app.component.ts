6dd1fc144bb2d4c133472b605e647eaa10afeeb4
kwietelmann <kaw57@calvin.edu>
2023-04-25 21:43:49 -0400

    documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,7 @@
 // // Brian Langejans: TheBguy87
 // // Kurt Wietelmann: kwietelmann
 // // 12/10/2022
-// import { Component, OnInit } from '@angular/core';
-// import { environment } from "../environments/environment";
-// import { getMessaging, getToken, onMessage } from "firebase/messaging";
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.scss']
-// })
-// export class AppComponent implements OnInit {
-// title = 'af-notification';
-//   message:any = null;
-//   constructor() {}
-//   ngOnInit(): void {
-//     this.requestPermission();
-//     this.listen();
-//   }
-//   requestPermission() {
-//     const messaging = getMessaging();
-//     getToken(messaging,
-//      { vapidKey: environment.firebase.vapidKey}).then(
-//        (currentToken) => {
-//          if (currentToken) {
-//            console.log("Hurraaa!!! we got the token.....");
-//            console.log(currentToken);
-//          } else {
-//            console.log('No registration token available. Request permission to generate one.');
-//          }
-//      }).catch((err) => {
-//         console.log('An error occurred while retrieving token. ', err);
-//     });
-//   }
-//   listen() {
-//     console.log("listening!")
-//     const messaging = getMessaging();
-//     onMessage(messaging, (payload) => {
-//       console.log('Message received. ', payload);
-//       this.message=payload;
-//     });
-//   }
-// }
+
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MessagingService } from './push.notification.service';
 
